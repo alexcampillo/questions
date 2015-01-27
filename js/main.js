8db07592f250fb83d64f941221e5fba1c0c96c6e
alexcampillo <acampillo@appdevy.com>
2015-01-27 12:13:00 -0600

    first init

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+//add questions
+	var questions = [
+		{
+			question: "Not too bad huh?",
+			choices: ["nope, not at all", "this is so great", "the universe salutes you"],
+			answer: 0
+		},
+		{
+			question: "How about another question?",
+			choices: ["tesla model s", "lots of money", "pizza"],
+			answer: 0
+		},
+		{
+			question: "More questions?",
+			choices: ["a", "b", "c"],
+			answer: 0
+		},
+		{
+			question: "How many questions are in this quiz?",
+			choices: ["yes", "no"],
+			answer: 0
+		},
+		{
+			question: "Is this it?",
+			choices: ["true", "false", "maybe", "the universe salutes you"],
+			answer: 0
+		},
+	]
+
+	var questionNumber = 0;
+	var answers = [];
+	var showQuestion = $('.showQuestion')
+	var showQuestionChoices = $('.showQuestionChoices')
+
+	showQuestion.text(questions[0].question)
+	for(i = 0; i < questions[0].choices.length; i++){
+			showQuestionChoices.append('<article class="choice row"><input class="left" type="radio"><p>' + questions[0].choices[i] + '</p></article>');
+		}
+
+//display questions one at a time
+	$('.nextQuestion').click(function() {
+		$('.showQuestionChoices .choice').remove();
+		questionNumber += 1;
+		var currentQ = questions[questionNumber];
+
+		showQuestion.text(currentQ.question);
+		console.log(currentQ.choices[questionNumber])
+
+		for(i = 0; i < currentQ.choices.length; i++){
+			showQuestionChoices.append('<article class="choice row"><input class="left" type="radio"><p>' + currentQ.choices[i] + '</p></article>');
+		}
+	});
+
+
+});

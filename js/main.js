674468a19fd9ff00a2a221d9522dc098a36467f4
alexcampillo <acampillo@appdevy.com>
2015-01-29 16:58:02 -0600

    added local storage

--- js/main.js
+++ js/main.js
@@ -4,27 +4,22 @@ $(document).ready(function() {
 		{
 			question: "Not too bad huh?",
 			choices: ["nope, not at all", "this is so great", "the universe salutes you"],
-			answer: 0
 		},
 		{
 			question: "How about another question?",
 			choices: ["tesla model s", "lots of money", "pizza"],
-			answer: 0
 		},
 		{
 			question: "More questions?",
 			choices: ["20 dollar salad", "tete", "Maria's"],
-			answer: 0
 		},
 		{
 			question: "How many questions are in this quiz?",
 			choices: ["fried mushroom", "no"],
-			answer: 0
 		},
 		{
 			question: "Is this it?",
 			choices: ["true", "false", "maybe", "the universe salutes you"],
-			answer: 0
 		},
 	]
 
@@ -35,22 +30,49 @@ $(document).ready(function() {
 
 	showQuestion.text(questions[0].question)
 	for(i = 0; i < questions[0].choices.length; i++){
-			showQuestionChoices.append('<article class="choice row"><input class="left" type="radio"><p>' + questions[0].choices[i] + '</p></article>');
+			showQuestionChoices.append('<article class="choice row"><input class="left" name="answer" type="radio" value="' +  questions[0].choices[i] + '"><p>' + questions[0].choices[i] + '</p></article>');
 		}
 
 //display questions one at a time
-	$('.nextQuestion').click(function() {
+	$('.nextQuestion').click(function(e) {
+		e.preventDefault();
+
+		choose();
+
 		$('.showQuestionChoices .choice').remove();
 		questionNumber += 1;
 		var currentQ = questions[questionNumber];
 
 		showQuestion.text(currentQ.question);
-		console.log(currentQ.choices[questionNumber])
 
 		for(i = 0; i < currentQ.choices.length; i++){
-			showQuestionChoices.append('<article class="choice row"><input class="left" type="radio"><p>' + currentQ.choices[i] + '</p></article>');
+			showQuestionChoices.append('<article class="choice row"><input class="left" name="answer" type="radio" value="' +  currentQ.choices[i] + '"><p>' + currentQ.choices[i] + '</p></article>');
 		}
+		
+		return false
 	});
 
+	function choose() {
+        choice = $('input[name="answer"]:checked').val();
+        answers.push(choice);
+        console.log(answers);
+    }
+// $('#next').on('click', function (e) {
+//         e.preventDefault();
+
+//         // Suspend click listener during fade animation
+//         if(quiz.is(':animated')) {        
+//             return false;
+//         }
+//         choose();
+
+//         // If no user selection, progress is stopped
+//         if (isNaN(selections[questionCounter])) {
+//             alert('Please make a selection!');
+//         } else {
+//             questionCounter++;
+//             displayNext();
+//         }
+//     });
 
 });
